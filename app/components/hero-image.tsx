1e79f3687bb80ade52cc4c863c889e1ca88beebc
sina <abf.sina@gmail.com>
2023-10-18 21:22:14 +0330

    add lines on the image

--- app/components/hero-image.tsx
+++ app/components/hero-image.tsx
@@ -1,67 +1,112 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import { useInView } from 'react-intersection-observer';
-import { twMerge } from 'tailwind-merge';
-import hero from '~/public/images/hero.webp';
+import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import { twMerge } from "tailwind-merge";
+import hero from "~/public/images/hero.webp";
+import { CSSProperties, useEffect, useRef, useState } from "react";
 
+interface Line {
+  id: string;
+  direction: "to bottom" | "to right";
+  size: number;
+  duration: number;
+}
 const HeroImage = () => {
   const { ref, inView } = useInView({
     threshold: 0.4,
-    triggerOnce: true
+    triggerOnce: true,
   });
 
+  const [lines, setLines] = useState<Line[]>([]);
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const randomNumberBetween = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const removeLine = (id: string) => {
+    setLines((prev) => prev.filter((line) => line.id !== id));
+  };
+
+  useEffect(() => {
+    if (!inView) return;
+    const renderLine = (timeout: number) => {
+      timeoutRef.current = setTimeout(() => {
+        setLines((lines) => [
+          ...lines,
+          {
+            direction: Math.random() > 0.5 ? "to bottom" : "to right",
+            duration: randomNumberBetween(1300, 3500),
+            size: randomNumberBetween(10, 30),
+            id: Math.random().toString(36).substring(7),
+          },
+        ]);
+        renderLine(randomNumberBetween(800, 2500));
+      }, timeout);
+    };
+
+    renderLine(randomNumberBetween(800, 1300));
+
+    return () => {
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    };
+  }, [inView]);
+
   return (
-    <div
-      ref={ref}
-      className='[perspective:2000px] py-[6.4rem] mt-[12.8rem]'
-    >
+    <div ref={ref} className="[perspective:2000px] py-[6.4rem] mt-[12.8rem]">
       <div
         className={twMerge(
-          'relative bg-hero-gradient border border-transparent-white bg-white bg-opacity-[0.01] rounded-lg',
-          inView ? 'animate-image-rotate' : '[transform:rotateX(25deg)]',
-          'before:w-full before:h-full before:top-0 before:left-0 before:absolute before:bg-hero-glow before:[filter:blur(120px)] before:opacity-0',
-          inView && 'before:animate-image-glow'
+          "relative bg-hero-gradient border border-transparent-white bg-white bg-opacity-[0.01] rounded-lg",
+          inView ? "animate-image-rotate" : "[transform:rotateX(25deg)]",
+          "before:w-full before:h-full before:top-0 before:left-0 before:absolute before:bg-hero-glow before:[filter:blur(120px)] before:opacity-0",
+          inView && "before:animate-image-glow",
         )}
       >
+        <div className="absolute w-full h-full top-0 left-0 z-20">
+          {lines.map((line) => (
+            <span
+              key={line.id}
+              onAnimationEnd={() => removeLine(line.id)}
+              style={
+                {
+                  "--direction": line.direction,
+                  "--size": line.size,
+                  "--animation-duration": `${line.duration}ms`,
+                } as CSSProperties
+              }
+              className={twMerge(
+                "bg-glow-lines block absolute top-0 w-[10rem] h-[1px]",
+                line.direction === "to bottom" &&
+                  "h-[calc(var(--size)*1rem)] w-[1px] right-0 animate-glow-line-vertical",
+                line.direction === "to right" &&
+                  "w-[calc(var(--size)*1rem)] h-[1px] left-0 animate-glow-line-horizontal",
+              )}
+            />
+          ))}
+        </div>
         <svg
           className={twMerge(
-            'absolute left-0 top-0 h-full w-full',
-            '[&_path]:stroke-white [&_path]:[stroke-opacity:0.2] [&_path]:[stroke-dasharray:1] [&_path]:[stroke-dashoffset:1]',
-            inView && '[&_path]:animate-sketch-lines'
+            "absolute left-0 top-0 h-full w-full",
+            "[&_path]:stroke-white [&_path]:[stroke-opacity:0.2] [&_path]:[stroke-dasharray:1] [&_path]:[stroke-dashoffset:1]",
+            inView && "[&_path]:animate-sketch-lines",
           )}
-          width='100%'
-          viewBox='0 0 1499 778'
-          fill='none'
+          width="100%"
+          viewBox="0 0 1499 778"
+          fill="none"
         >
-          <path
-            pathLength='1'
-            d='M1500 72L220 72'
-          ></path>
-          <path
-            pathLength='1'
-            d='M1500 128L220 128'
-          ></path>
-          <path
-            pathLength='1'
-            d='M1500 189L220 189'
-          ></path>
-          <path
-            pathLength='1'
-            d='M220 777L220 1'
-          ></path>
-          <path
-            pathLength='1'
-            d='M538 777L538 128'
-          ></path>
+          <path pathLength="1" d="M1500 72L220 72"></path>
+          <path pathLength="1" d="M1500 128L220 128"></path>
+          <path pathLength="1" d="M1500 189L220 189"></path>
+          <path pathLength="1" d="M220 777L220 1"></path>
+          <path pathLength="1" d="M538 777L538 128"></path>
         </svg>
         <Image
           className={twMerge(
-            'relative z-10 transition-opacity delay-[680ms]',
-            inView ? 'opacity-100' : 'opacity-0'
+            "relative z-10 transition-opacity delay-[680ms]",
+            inView ? "opacity-100" : "opacity-0",
           )}
           src={hero}
-          alt='hero image'
+          alt="hero image"
         />
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,42 @@
-import Image from 'next/image';
-import Hero from './components/hero';
-import Button from './components/ui/button';
-import Container from './components/container';
-import { ArrowIcon } from './components/icons';
-import HeroImage from './components/hero-image';
+import Hero from "./components/hero";
+import Button from "./components/ui/button";
+import Container from "./components/container";
+import { ArrowIcon } from "./components/icons";
+import HeroImage from "./components/hero-image";
 export default function Home() {
   return (
-    <Container className='mt-[6.4rem] overflow-hidden'>
+    <Container className="mt-[6.4rem]">
       <Hero>
         <Button
-          href='/'
-          variant='secondary'
-          size='small'
-          className='animate-fade-in opacity-0 translate-y-[-1rem]'
+          href="/"
+          variant="secondary"
+          size="small"
+          className="animate-fade-in opacity-0 translate-y-[-1rem]"
         >
-          <span className='font-medium'>Announcing our $35M Series B</span>
-          <span className='-mr-[0.6rem] ml-1'>
+          <span className="font-medium">Announcing our $35M Series B</span>
+          <span className="-mr-[0.6rem] ml-1">
             <ArrowIcon />
           </span>
         </Button>
-        <Hero.Title className='animate-fade-in [--animation-delay:200ms] opacity-0 translate-y-[-1rem]'>
-          Linear is a better way <br className='hidden md:block' /> to build
+        <Hero.Title className="animate-fade-in [--animation-delay:200ms] opacity-0 translate-y-[-1rem]">
+          Linear is a better way <br className="hidden md:block" /> to build
           products
         </Hero.Title>
 
-        <Hero.Subtitle className='animate-fade-in [--animation-delay:400ms] opacity-0 translate-y-[-1rem]'>
+        <Hero.Subtitle className="animate-fade-in [--animation-delay:400ms] opacity-0 translate-y-[-1rem]">
           Meet the new standard for modern software development.
-          <br className='hidden md:block' />
+          <br className="hidden md:block" />
           Streamline issues, sprints, and product roadmaps.
         </Hero.Subtitle>
         <Button
-          href='/'
-          variant='primary'
-          size='large'
-          className='animate-fade-in [--animation-delay:600ms] opacity-0 translate-y-[-1rem]'
+          href="/"
+          variant="primary"
+          size="large"
+          className="animate-fade-in [--animation-delay:600ms] opacity-0 translate-y-[-1rem]"
         >
           Get started
-          <span className='-mr-[0.6rem] ml-1 text-off-white'>
-            <ArrowIcon className='w-[1.6rem] h-[1.6rem]' />
+          <span className="-mr-[0.6rem] ml-1 text-off-white">
+            <ArrowIcon className="w-[1.6rem] h-[1.6rem]" />
           </span>
         </Button>
         <HeroImage />
